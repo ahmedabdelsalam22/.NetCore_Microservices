269848deb92beeaf42fb41cd4097222dd247bc7a
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-09-04 15:50:43 +0300

    add IHTTPClientFactory confoguration in program cs and add couponApi url in App.json

--- Mango.Web/Service/IService/ICouponService.cs
+++ Mango.Web/Service/IService/ICouponService.cs
@@ -4,11 +4,11 @@ namespace Mango.Web.Service.IService
 {
     public interface ICouponService
     {
-        Task<ResponseDTO> GetCouponById(int couponId);
-        Task<ResponseDTO> GetCouponByCoupnCode(string couponCode);
-        Task<ResponseDTO> GetAllCoupons();
-        Task<ResponseDTO> CreateCoupon(CouponDTO couponDTO);
-        Task<ResponseDTO> UpdateCoupon(CouponDTO couponDTO);
-        Task<ResponseDTO> DeleteCoupon(int couponId);
+        Task<ResponseDTO?> GetCouponById(int couponId);
+        Task<ResponseDTO?> GetCouponByCoupnCode(string couponCode);
+        Task<ResponseDTO?> GetAllCoupons();
+        Task<ResponseDTO?> CreateCoupon(CouponDTO couponDTO);
+        Task<ResponseDTO?> UpdateCoupon(CouponDTO couponDTO);
+        Task<ResponseDTO?> DeleteCoupon(int couponId);
     }
 }
