93523705b56d7ec12df5cdf026d7bbc806ed9104
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-09-04 14:26:48 +0300

    create RequestDTO and ApiType enum

---
+++ Mango.Web/Models/ResponseDTO.cs
@@ -0,0 +1,11 @@
+﻿using System.Net;
+
+namespace Mango.Services.CouponAPI.Models
+{
+    public class ResponseDTO
+    {
+        public bool IsSuccess { get; set; }
+        public Object? Result { get; set; }
+        public List<string>? ErrorMessage { get; set; }
+    }
+}
