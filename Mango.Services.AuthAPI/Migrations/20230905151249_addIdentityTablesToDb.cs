eefd18f65ea4b9e314bd8b1f4b0cc027b19e856c
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-09-05 18:13:30 +0300

    add IdentityMigration to db

--- Mango.Services.AuthAPI/Migrations/20230905151249_addIdentityTablesToDb.cs
+++ Mango.Services.AuthAPI/Migrations/20230905151249_addIdentityTablesToDb.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace Mango.Services.AuthAPI.Migrations
 {
     /// <inheritdoc />
-    public partial class AddIdentityTables : Migration
+    public partial class addIdentityTablesToDb : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
