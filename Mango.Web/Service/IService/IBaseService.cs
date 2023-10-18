6d07cd7baacc63f63bf0ba332135e4420de2c0ee
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-10-18 04:01:13 +0300

    consume login and register using restSharp successfully

--- Mango.Web/Service/IService/IBaseService.cs
+++
@@ -1,9 +0,0 @@
-﻿using Mango.Web.Models.DTOS;
-
-namespace Mango.Web.Service.IService
-{
-    public interface IBaseService
-    {
-        Task<ResponseDTO?> SendAsync(RequestDTO requestDTO);
-    }
-}
