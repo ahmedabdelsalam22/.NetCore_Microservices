e42bb9467763a648862f4993547828fdd42e13ca
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-10-18 04:53:23 +0300

    some refactoies

--- Mango.Web/Models/DTOS/RegisterRequestDTO.cs
+++ Mango.Web/Models/DTOS/RegisterRequestDTO.cs
@@ -14,7 +14,6 @@ namespace Mango.FrontEnd.Models.DTOS
         public string Email { get; set; }
         [Required]
         [MinLength(8, ErrorMessage = "password must contains 8 characters and numbers at least")]
-        [RegularExpression("([a-z]|[A-Z]|[0-9]){4}[a-zA-Z0-9\\W]{3,11}", ErrorMessage = "password must contains capital letters")]
         public string Password { get; set; }
     }
 }
