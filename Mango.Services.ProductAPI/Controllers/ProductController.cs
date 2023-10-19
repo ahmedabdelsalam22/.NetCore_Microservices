2862dcaf0301c66969482a1c7ea3a7d6dd8de280
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-10-19 19:10:47 +0300

    create ProductController and write logic for "GetAllProducts"

---
+++ Mango.Services.ProductAPI/Controllers/ProductController.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using Mango.Services.CouponAPI.Repository.IRepository;
+using Mango.Services.ProductAPI.Models;
+using Mango.Services.ProductAPI.Models.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.ProductAPI.Controllers
+{
+    [Route("api/")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductRepository _productRepository;
+        private readonly IMapper _mapper;
+
+        public ProductController(IProductRepository productRepository, IMapper mapper)
+        {
+            _productRepository = productRepository;
+            _mapper = mapper;
+        }
+
+        [HttpGet("products")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> GetAllProducts() 
+        {
+            try
+            {
+                IEnumerable<Product> products = await _productRepository.GetAll();
+
+                if (products == null)
+                {
+                    return NotFound("no data found");
+                }
+
+                List<ProductDto> productDtos = _mapper.Map<List<ProductDto>>(products);
+                return Ok(productDtos);
+            }
+            catch (Exception ex) 
+            {
+                return BadRequest(ex.ToString());
+            }
+        }
+    }
+}
