f3f415d35f77c2233e41b2878971defe1e2262dc
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-10-19 18:54:28 +0300

    create IRepository and Repository

---
+++ Mango.Services.ProductAPI/Services/IRepositories/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq.Expressions;
+
+namespace Mango.Services.ProductAPI.Services.IRepositories
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<List<T>> GetAll(bool tracked = true);
+        Task<T> Get(Expression<Func<T, bool>>? filter = null, bool tracked = true);
+        Task Create(T entity);
+        void Delete(T entity);
+    }
+}
