d8f83e287f5cf01b59d5dc0c74aee04ece800f60
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-10-20 17:57:48 +0300

    add ApplicationDbContext and all Program cs configs and add first migration

--- Mango.Services.ShoppingCartAPI/Program.cs
+++ Mango.Services.ShoppingCartAPI/Program.cs
@@ -1,11 +1,55 @@
+using Mango.Services.ShoppingCartAPI.Extensions;
+using Mango.Services.ShoppingCartAPI;
+using Mango.Services.ShoppingCartAPI.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(option => {
+
+    option.AddSecurityDefinition(
+        name: JwtBearerDefaults.AuthenticationScheme,
+        securityScheme: new OpenApiSecurityScheme
+        {
+            Name = "Authorization",
+            Description = "Enter the bearer authoriaztion as following `Bearer token`",
+            In = ParameterLocation.Header,
+            Type = SecuritySchemeType.ApiKey,
+            Scheme = "Bearer"
+        });
+    option.AddSecurityRequirement(new OpenApiSecurityRequirement {
+        {
+            new OpenApiSecurityScheme{
+            Reference=new OpenApiReference{
+                Type = ReferenceType.SecurityScheme,
+                Id = JwtBearerDefaults.AuthenticationScheme
+               }
+            },
+            new string[]{ }
+        }
+    });
+
+});
+
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(option => option.UseSqlServer(connectionString: connectionString));
+
+builder.Services.AddAutoMapper(typeof(MappingConfig));
+
+
+builder.AddAppAuthintication();
+
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
@@ -17,7 +61,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
