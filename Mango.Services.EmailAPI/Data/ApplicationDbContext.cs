8660bf75f48aed153cc2a8a53d7ee531761da8f9
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-10-23 00:59:37 +0300

    connect with db and add new migration

---
+++ Mango.Services.EmailAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Mango.Services.EmailAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.EmailAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<EmailLogger> EmailLoggers { get; set; }
+    }
+}
