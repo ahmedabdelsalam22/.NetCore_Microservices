03a86cdf94d352f61703efb587a384227ba28a6b
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-10-24 18:35:35 +0300

    settup orderAPI and add new migration

---
+++ Mango.Services.OrderAPI/Extensions/WebApplicationBuilderExtensions.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Mango.Services.OrderAPI.Extensions
+{
+    public static class WebApplicationBuilderExtensions
+    {
+        public static WebApplicationBuilder AddAppAuthintication(this WebApplicationBuilder builder)
+        {
+
+            var secret = builder.Configuration.GetValue<string>("ApiSetting:SecretKey");
+            var issuer = builder.Configuration.GetValue<string>("ApiSetting:Issuer");
+            var audience = builder.Configuration.GetValue<string>("ApiSetting:Audience");
+
+
+            var key = Encoding.ASCII.GetBytes(secret);
+
+
+            builder.Services.AddAuthentication(x => {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+
+            }).AddJwtBearer(x => {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = true,
+                    ValidIssuer = issuer,
+                    ValidateAudience = true,
+                    ValidAudience = audience
+                };
+            });
+            return builder;
+        }
+    }
+}
---
+++ Mango.Services.OrderAPI/Services/IServices/IProductRestService.cs
@@ -0,0 +1,9 @@
+﻿using Mango.Services.OrderAPI.Models.Dtos;
+
+namespace Mango.Services.OrderAPI.Services.IServices
+{
+    public interface IProductRestService
+    {
+        Task<List<ProductDto>> GetAllProducts();
+    }
+}
