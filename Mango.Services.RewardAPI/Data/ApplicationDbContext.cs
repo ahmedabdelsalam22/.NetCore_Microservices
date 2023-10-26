9f364ffdd4f87fe2c91708e27a3e5fc3438d5e0d
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-10-27 00:42:06 +0200

    connect with db and add Rewards Table

---
+++ Mango.Services.RewardAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Mango.Services.RewardAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.RewardAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Rewards> Rewards { get; set; }
+    }
+}
---
+++ Mango.Services.RewardAPI/Models/Rewards.cs
@@ -0,0 +1,10 @@
+﻿namespace Mango.Services.RewardAPI.Models
+{
+    public class Rewards
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public DateTime RewardsActivity { get; set; }
+        public int OrderId { get; set; }
+    }
+}
