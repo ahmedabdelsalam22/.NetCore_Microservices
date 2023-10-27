5720bbf41c89fbdb0be1da0622f3a7588db1baa4
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-10-28 00:49:19 +0200

    add Authintication to API Gateway

--- Mango.GatewaySolution/Program.cs
+++ Mango.GatewaySolution/Program.cs
@@ -1,10 +1,16 @@
+using Mango.GatewaySolution.Extensions;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+
+builder.AddAppAuthintication();
 
 builder.Services.AddOcelot();
+
+
+var app = builder.Build();
+
 
 app.MapGet("/", () => "Hello World!");
 app.UseOcelot();
