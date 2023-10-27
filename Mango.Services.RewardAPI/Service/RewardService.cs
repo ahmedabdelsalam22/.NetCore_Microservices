518585e6bbe2729f90b63cc856c7b96037ef3b48
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-10-27 03:04:20 +0200

    create IRewardService to store Rewards in db

---
+++ Mango.Services.RewardAPI/Service/RewardService.cs
@@ -0,0 +1,37 @@
+﻿using Mango.Services.RewardAPI.Data;
+using Mango.Services.RewardAPI.Message;
+using Mango.Services.RewardAPI.Models;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Mango.Services.RewardAPI.Service
+{
+    public class RewardService : IRewardService
+    {
+        private DbContextOptions<ApplicationDbContext> _dbOptions;
+
+        public RewardService(DbContextOptions<ApplicationDbContext> dbOptions)
+        {
+            this._dbOptions = dbOptions;
+        }
+
+        public async Task UpdateRewards(RewardsMessage rewardsMessage)
+        {
+            Rewards rewards = new() 
+            {
+                OrderId = rewardsMessage.OrderId,
+                UserId = rewardsMessage.UserId,
+                RewardsActivity = rewardsMessage.RewardsActivity,
+                RewardsDate = DateTime.Now
+            };
+
+            await using var _db = new ApplicationDbContext(_dbOptions);
+            await _db.Rewards.AddAsync(rewards);
+            await _db.SaveChangesAsync();
+        }
+
+    }
+}
