5ccedaeda4ac9ee6353855860f46c61611935e4f
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-10-28 03:34:19 +0200

    separate ocelot route in separation file and configure that in program cs

--- Mango.GatewaySolution/Program.cs
+++ Mango.GatewaySolution/Program.cs
@@ -6,12 +6,13 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.AddAppAuthintication();
 
-builder.Services.AddOcelot();
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+builder.Services.AddOcelot(builder.Configuration);
 
 
 var app = builder.Build();
 
 
 app.MapGet("/", () => "Hello World!");
-app.UseOcelot();
+app.UseOcelot().GetAwaiter().GetResult();
 app.Run();
